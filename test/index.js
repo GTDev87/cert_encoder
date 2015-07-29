b911e8dda576f272eb3af375a703f7484e81ef96
Gregory Thompson <gregt87@gmail.com>
2015-07-29 18:51:44 -0400

    initial commit of npm module infastructure

---
+++ test/index.js
@@ -0,0 +1,9 @@
+'use strict';
+var assert = require('assert');
+var certEncoding = require('../lib');
+
+describe('cert-encoding', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
